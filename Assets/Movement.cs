4bd5a91a8aa5779c42149903b7db5230c308aeaa
Brunookamoto157 <81862704+Brunookamoto157@users.noreply.github.com>
2024-10-16 21:40:52 -0300

    att 16-10-24

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -7,7 +7,7 @@ using UnityEngine.AI;
 public class Movement : MonoBehaviour
 {
     public NavMeshAgent agent;
-    public float rotateSpeedMovment = 0.05f;
+    public float rotateSpeedMovement = 0.05f;
     private float rotateVelocity;
 
     public Animator anim;
@@ -16,34 +16,44 @@ public class Movement : MonoBehaviour
     void Start()
     {
         agent = gameObject.GetComponent<NavMeshAgent>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     void Update()
     {
         Animation();
         Move();
-
     }
 
     public void Animation()
     {
         float speed = agent.velocity.magnitude / agent.speed;
         anim.SetFloat("Speed", speed, motionSmoothTime, Time.deltaTime);
-
     }
 
     public void Move()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1)) // Detecta clique do botão direito do mouse
         {
             RaycastHit hit;
 
+            // Lança um raio a partir da posição do mouse para o mundo 3D
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
             {
-                if (hit.collider.tag == "Ground") { }
+                // Verifica se o raio colidiu com o chão (tag "Ground")
+                if (hit.collider.tag == "Ground")
+                {
+                    // Define o destino do NavMeshAgent como o ponto onde o clique aconteceu
+                    agent.SetDestination(hit.point);
+                    agent.stoppingDistance = 0;
+
+                    Quaternion rotationToLookAt = Quaternion.LookRotation(hit.point - transform.position);
+                    float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotationToLookAt.eulerAngles.y,
+                        ref rotateVelocity, rotateSpeedMovement * (Time.deltaTime * 5));
+
+                    transform.eulerAngles = new Vector3(0, rotationY, 0);
+                }
             }
         }
-
     }
-
 }
